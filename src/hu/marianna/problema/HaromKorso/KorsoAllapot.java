82ddfac316816e8428f5a7b00258600761525926
Marianna Szabó <szabo.marica@gmail.com>
2017-05-03 23:16:39 +0200

    Heuristic function updated

--- src/hu/marianna/problema/HaromKorso/KorsoAllapot.java
+++ src/hu/marianna/problema/HaromKorso/KorsoAllapot.java
@@ -52,7 +52,7 @@ public class KorsoAllapot implements Allapot {
       return 0;
     }
 
-    return 40000 - (a[0] > 0 ? 1 : 0) - (a[1] > 0 ? 1 : 0) - (a[2] > 0 ? 1 : 0);
+    return 4 - (a[0] > 0 ? 1 : 0) - (a[1] > 0 ? 1 : 0) - (a[2] > 0 ? 1 : 0);
   }
 
 }
